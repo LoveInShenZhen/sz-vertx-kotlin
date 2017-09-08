25b52f305973511454860a2bf82f7296dbbf541a
KK <kk@KK-MBP.local>
2017-09-08 14:23:22 +0800

    refactor

--- subProjects/sz-scaffold/src/main/kotlin/sz/scaffold/controller/builtIn/Default.kt
+++ subProjects/sz-scaffold/src/main/kotlin/sz/scaffold/controller/builtIn/Default.kt
@@ -5,7 +5,7 @@ import sz.scaffold.controller.ApiController
 //
 // Created by kk on 17/9/8.
 //
-class DefaultRoot : ApiController() {
+class Default : ApiController() {
 
     fun hello() : String {
         this.contentType("text/plain")
