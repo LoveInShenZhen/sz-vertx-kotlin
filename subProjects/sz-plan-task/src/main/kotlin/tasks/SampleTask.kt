4d3b567522f9c93098c45ec3423aa15086df1431
KK <kk@KK-MBP.local>
2017-09-11 10:55:26 +0800

    add time field

--- subProjects/sz-plan-task/src/main/kotlin/tasks/SampleTask.kt
+++ subProjects/sz-plan-task/src/main/kotlin/tasks/SampleTask.kt
@@ -1,5 +1,6 @@
 package tasks
 
+import jodd.datetime.JDateTime
 import sz.scaffold.tools.logger.AnsiColor
 import sz.scaffold.tools.logger.Logger
 
@@ -7,6 +8,9 @@ import sz.scaffold.tools.logger.Logger
 // Created by kk on 17/8/27.
 //
 class SampleTask : Runnable{
+
+    val time = JDateTime()
+
     override fun run() {
         Logger.debug("Run SampleTask", AnsiColor.YELLOW)
     }
