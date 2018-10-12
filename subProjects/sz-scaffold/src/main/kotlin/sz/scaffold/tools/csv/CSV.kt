2d59fcfb43647bfe9cc7a491a0e10b01a7eea76e
kklongming <kklongmono@gmail.com>
2018-10-12 11:50:37 +0800

    bug fix and add loadFromFile(...) method.

--- subProjects/sz-scaffold/src/main/kotlin/sz/scaffold/tools/csv/CSV.kt
+++ subProjects/sz-scaffold/src/main/kotlin/sz/scaffold/tools/csv/CSV.kt
@@ -8,10 +8,8 @@ import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import jodd.datetime.JDateTime
 import sz.scaffold.tools.json.JDateTimeJsonDeserializer
 import sz.scaffold.tools.json.JDateTimeJsonSerializer
-import sz.scaffold.tools.logger.AnsiColor
-import sz.scaffold.tools.logger.Logger
 import java.io.File
-import java.io.FileWriter
+import java.io.FileOutputStream
 
 //
 // Created by kk on 2018/10/11.
@@ -34,17 +32,32 @@ object CSV {
     fun saveToFile(csvFilePath: String, beans: List<*>, clazz: Class<*>, append: Boolean = false) {
         val csvSchema = mapper.schemaFor(clazz)
         val destFile = File(csvFilePath)
-        val needWriteHeader = append.not() || destFile.length() == 0L
+        val needWriteHeader = append.not() || destFile.exists().not() || destFile.length() == 0L
+
+        val fos = FileOutputStream(destFile, append).buffered()
 
         val objw = if (needWriteHeader) {
-            mapper.writerFor(clazz).with(csvSchema.withHeader()).writeValues(destFile.bufferedWriter())
+            mapper.writerFor(clazz).with(csvSchema.withHeader()).writeValues(fos)
         } else {
-            mapper.writerFor(clazz).with(csvSchema).writeValues(destFile.bufferedWriter())
+            mapper.writerFor(clazz).with(csvSchema).writeValues(fos)
         }
 
         objw.use {
             objw.writeAll(beans)
         }
     }
+
+    inline fun <reified T : Any>  saveToFile(csvFilePath: String, beans: List<T>, append: Boolean = false) {
+        saveToFile(csvFilePath, beans, T::class.java, append)
+    }
+
+    inline fun <reified T : Any> loadFromFile(csvFilePath: String): List<T> {
+        val csvSchema = mapper.schemaFor(T::class.java).withHeader()
+        val destFile = File(csvFilePath)
+
+        val it = mapper.readerFor(T::class.java).with(csvSchema).readValues<T>(destFile.bufferedReader())
+        return it.readAll()
+    }
+
 
 }
