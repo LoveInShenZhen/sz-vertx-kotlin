1b0ec516e4a4a7de0b47471364ff13cb5caf9044
kklongming <kklongmono@gmail.com>
2019-06-12 20:15:46 +0800

    1. add method: awaitEventWithTimeout 2. add method: awaitResultWithTimeout 3. add config: operationTimeout

--- subProjects/sz-scaffold/src/main/kotlin/sz/scaffold/coroutines/Ext.kt
+++ subProjects/sz-scaffold/src/main/kotlin/sz/scaffold/coroutines/Ext.kt
@@ -1,10 +1,15 @@
 package sz.scaffold.coroutines
 
+import io.vertx.core.AsyncResult
+import io.vertx.core.Handler
 import io.vertx.core.Vertx
+import io.vertx.kotlin.coroutines.awaitEvent
+import io.vertx.kotlin.coroutines.awaitResult
 import io.vertx.kotlin.coroutines.dispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
 import sz.scaffold.Application
 
 //
@@ -13,4 +18,12 @@ import sz.scaffold.Application
 
 fun CoroutineScope.launchOnVertx(vertx: Vertx = Application.vertx, block: suspend CoroutineScope.() -> Unit): Job {
     return this.launch(context = vertx.dispatcher(), block = block)
+}
+
+suspend fun <T> awaitEventWithTimeout(timeOut: Long, block: (h: Handler<T>) -> Unit): T = withTimeout(timeOut) {
+    awaitEvent(block)
+}
+
+suspend fun <T> awaitResultWithTimeout(timeOut: Long, block: (h: Handler<AsyncResult<T>>) -> Unit): T = withTimeout(timeOut) {
+    awaitResult(block)
 }
