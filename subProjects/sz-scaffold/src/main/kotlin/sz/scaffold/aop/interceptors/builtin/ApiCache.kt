e3722cf4376a7ef1ece722c79c60b7d1e49b1306
kklongming <kklongmono@gmail.com>
2019-06-28 22:23:28 +0800

    实现 拦截器 api 缓存

---
+++ subProjects/sz-scaffold/src/main/kotlin/sz/scaffold/aop/interceptors/builtin/ApiCache.kt
@@ -0,0 +1,64 @@
+package sz.scaffold.aop.interceptors.builtin
+
+//
+// Created by kk on 2019-06-28.
+//
+
+import io.vertx.core.http.HttpMethod
+import jodd.crypt.DigestEngine
+import sz.scaffold.aop.actions.Action
+import sz.scaffold.aop.annotations.WithAction
+import sz.scaffold.cache.redis.RedisCacheApi
+import sz.scaffold.controller.ContentTypes
+import sz.scaffold.tools.json.toShortJson
+import sz.scaffold.tools.logger.Logger
+
+@WithAction(ApiCacheAction::class)
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApiCache(
+    val expireTimeInMs: Long,
+    val cacheName: String = "default",
+    val excludeQueryParams: Array<String> = arrayOf("_")
+)
+
+class ApiCacheAction : Action<ApiCache>() {
+
+    override suspend fun call(): Any? {
+        val request = this.httpContext.request()
+        val excludes = this.config.excludeQueryParams.toSet()
+        val queryParamsTxt = request.params()
+            .filter { excludes.contains(it.key).not() }
+            .map { "${it.key}=${it.value}" }
+            .joinToString("&")
+
+        val bodyParams = if (request.method() == HttpMethod.POST) {
+            this.httpContext.bodyAsString + request.formAttributes().joinToString("&") { "${it.key}=${it.value}" }
+        } else {
+            ""
+        }
+
+        Logger.debug("queryParamsTxt: $queryParamsTxt")
+        Logger.debug("bodyParams: $bodyParams")
+
+        val cacheKey = "ApiCache@${request.path()}@${request.method().name}@${DigestEngine.sha1().digestString(queryParamsTxt + bodyParams)}"
+        Logger.debug("cacheKey: $cacheKey")
+        val cache = RedisCacheApi(this.config.cacheName)
+        val cacheValue = cache.getOrNullAwait(cacheKey)
+
+        if (cacheValue == null) {
+            val result = delegate.call()
+            result?.let {
+                cache.setAwait(cacheKey, it.toShortJson(), this.config.expireTimeInMs)
+                Logger.debug("缓存Api结果,expire: ${this.config.expireTimeInMs}, key: $cacheKey")
+            }
+            return result
+        } else {
+            val response = this.httpContext.response()
+            response.putHeader("Content-Type", ContentTypes.Json)
+            response.write(cacheValue)
+            Logger.debug("直接获取缓存结果:\n${cacheValue}")
+            return null
+        }
+    }
+}
