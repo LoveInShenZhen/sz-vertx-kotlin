8ff4fe7f78d395918980087062d717c26b342568
kklongming <kklongmono@gmail.com>
2019-11-25 23:02:11 +0800

    add BeanMessageCodec

---
+++ subProjects/sz-scaffold/src/main/kotlin/sz/scaffold/eventbus/BeanMessageCodec.kt
@@ -0,0 +1,41 @@
+package sz.scaffold.eventbus
+
+import io.netty.util.CharsetUtil
+import io.vertx.core.buffer.Buffer
+import io.vertx.core.eventbus.MessageCodec
+import sz.scaffold.tools.json.Json
+import sz.scaffold.tools.json.toShortJson
+
+//
+// Created by kk on 2019/11/25.
+//
+class BeanMessageCodec<BeanType : Any>(private val beanClass: Class<BeanType>) : MessageCodec<BeanType, BeanType> {
+
+    override fun decodeFromWire(pos: Int, buffer: Buffer): BeanType {
+        val length = buffer.getInt(pos)
+        val startPos = pos + 4
+        val bytes = buffer.getBytes(startPos, startPos + length)
+        val jsonStr = String(bytes, CharsetUtil.UTF_8)
+        return Json.fromJsonString(jsonStr, beanClass)
+    }
+
+    override fun systemCodecID(): Byte {
+        return -1
+    }
+
+    override fun encodeToWire(buffer: Buffer, s: BeanType) {
+        val jsonStr = s.toShortJson()
+        val strBytes: ByteArray = jsonStr.toByteArray(CharsetUtil.UTF_8)
+        buffer.appendInt(strBytes.size)
+        buffer.appendBytes(strBytes)
+    }
+
+    override fun transform(s: BeanType): BeanType {
+        return s
+    }
+
+    override fun name(): String {
+        return "BeanMessageCodec#${beanClass.name}"
+    }
+
+}
