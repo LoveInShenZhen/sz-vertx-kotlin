18ea309c89b2c911b240e67c9a1e4e6bfdabc503
kklongming <kklongmono@gmail.com>
2019-11-28 00:18:20 +0800

    upgrade kotlin to 1.3.61 upgrade kotlinx-coroutines-* to 1.3.2

--- subProjects/sz-tools/build.gradle.kts
+++ subProjects/sz-tools/build.gradle.kts
@@ -9,12 +9,11 @@ plugins {
 dependencies {
     api(kotlin("stdlib-jdk8"))
     api(kotlin("reflect"))
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.2")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.2.2")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.3.2")
 
     // android gradle依赖：implementation 和compile的区别
     // 参考: https://www.jianshu.com/p/f34c179bc9d0 根据需要选择使用不同的依赖设定方式
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
     api(project(":subProjects:jodd-dependency"))
 
     api("com.fasterxml.jackson.module:jackson-module-jsonSchema:2.10.1")
