82c1418ac246cb2b1402f7c2ad62cc6eaabebf91
kklongming <kklongmono@gmail.com>
2019-12-28 23:12:21 +0800

    add comments

--- subProjects/sz-scaffold/src/main/kotlin/sz/scaffold/aop/interceptors/profiler/ApiProfiler.kt
+++ subProjects/sz-scaffold/src/main/kotlin/sz/scaffold/aop/interceptors/profiler/ApiProfiler.kt
@@ -7,6 +7,20 @@ import sz.scaffold.tools.logger.Logger
 //
 // Created by kk on 2019-06-27.
 //
+
+// app.httpServer.interceptors 中的配置
+// includes 和 excludes 采用通配符匹配, excludes 比 includes 的优先级高
+// Possible patterns allow to match single characters ('?') or any count of characters ('*').
+// Wildcard characters can be escaped (by an '\'). When matching path, deep tree wildcard also can be used ('**').
+//{
+//    className = "sz.scaffold.aop.interceptors.profiler.ApiProfiler"
+//    config = {
+//        timeThreshold = 100
+//        includes = ["/**"]
+//        excludes = []
+//    }
+//}
+
 class ApiProfiler : GlobalInterceptorBase() {
 
     private val logger = Logger.of("sz.api.profiler")
