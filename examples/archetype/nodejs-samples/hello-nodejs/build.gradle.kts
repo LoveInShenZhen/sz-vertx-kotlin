c647e137d59407250c14b7e3dadf61c737334d41
kklongming <kklongmono@gmail.com>
2021-07-18 12:34:48 +0800

    添加 sample

---
+++ examples/archetype/nodejs-samples/hello-nodejs/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("org.jetbrains.kotlin.js") version "1.5.21"
+}
+
+group = "org.sz.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-js"))
+    implementation("org.jetbrains.kotlinx:kotlinx-nodejs:0.0.7")
+}
+
+kotlin {
+    js {
+        nodejs {
+        }
+        binaries.executable()
+    }
+}
---
+++ examples/archetype/nodejs-samples/hello-nodejs/src/main/kotlin/org/sz/examples/app.kt
@@ -0,0 +1,10 @@
+package org.sz.examples
+
+
+//
+// Created by kk on 2021/7/18.
+//
+
+fun main() {
+    println("hello nodejs")
+}
