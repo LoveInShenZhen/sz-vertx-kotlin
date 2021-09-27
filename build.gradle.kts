abcce8c3abd6f3154f19b67fc2e976ad427331fb
KK <long.ming@myquant.cn>
2021-09-27 14:09:57 +0800

    upgrade kotlin to 1.5.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
-    kotlin("jvm").version("1.5.30").apply(false)
+    kotlin("jvm").version("1.5.31").apply(false)
 }
 
 allprojects {
