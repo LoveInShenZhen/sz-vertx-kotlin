17020936a9abae37a3d7c3aa0dc08eabd9ac68b5
kklongming <kklongmono@gmail.com>
2021-10-07 10:44:28 +0800

    add sample project

---
+++ examples/archetype/cliapp/src/main/kotlin/sz/cli/config/config.kt
@@ -0,0 +1,11 @@
+package sz.cli.config
+
+import com.typesafe.config.ConfigFactory
+import io.github.config4k.extract
+
+//
+// Created by kk on 2021/10/4.
+//
+
+val config = ConfigFactory.defaultApplication()
+
