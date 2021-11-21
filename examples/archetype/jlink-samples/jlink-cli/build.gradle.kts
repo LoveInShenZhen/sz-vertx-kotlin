90433f5d4f4f974fd69161a14ebf9c8421f9a194
kklongming <kklongmono@gmail.com>
2021-11-21 16:32:23 +0800

    cleanup and upgrade dependencies

--- examples/archetype/jlink-samples/jlink-cli/build.gradle.kts
+++ examples/archetype/jlink-samples/jlink-cli/build.gradle.kts
@@ -2,13 +2,16 @@ plugins {
     kotlin("jvm") version "1.6.0"
     java
     application
-    id("org.beryx.runtime") version "1.12.6"
+    id("org.beryx.runtime") version "1.12.7"
 }
 
 group = "com.github.kklongming"
 version = "1.0.0"
 
 repositories {
+    maven {
+        url= uri("https://maven.aliyun.com/repository/public/")
+    }
     mavenCentral()
 }
 
@@ -17,13 +20,13 @@ dependencies {
 //    implementation(kotlin("reflect"))
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("com.github.ajalt.clikt:clikt:3.2.0") {
+    implementation("com.github.ajalt.clikt:clikt:3.3.0") {
         exclude(group = "org.jetbrains.kotlin")
     }
-    implementation("ch.qos.logback:logback-classic:1.2.6")
+    implementation("ch.qos.logback:logback-classic:1.2.7")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.getByName<Test>("test") {
