d2034d0380879329eb63dc12b316c865e6045cb9
kklongming <kklongmono@gmail.com>
2022-04-17 15:43:35 +0800

    upgrade kotlinx-coroutines-jdk8 to 1.6.1 upgrade jackson to 2.13.2

--- subProjects/sz-tools/build.gradle.kts
+++ subProjects/sz-tools/build.gradle.kts
@@ -7,26 +7,29 @@ plugins {
 }
 
 dependencies {
-//    api(kotlin("stdlib-jdk8"))
     api(kotlin("reflect"))
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.5.2")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.1")
 
     // android gradle依赖：implementation 和compile的区别
     // 参考: https://www.jianshu.com/p/f34c179bc9d0 根据需要选择使用不同的依赖设定方式
     api(project(":subProjects:jodd-dependency"))
     api(project(":subProjects:sz-log"))
 
-    api("com.fasterxml.jackson.module:jackson-module-jsonSchema:2.12.2")
-    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.2") {
+    api("com.fasterxml.jackson.module:jackson-module-jsonSchema:2.13.2")
+    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2") {
         this.exclude(group = "org.jetbrains.kotlin")
     }
-    api("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.12.2")
-    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.2")
-    api("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.12.2")
-    api("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.12.2")
+    api("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.13.2")
+    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.2")
+    api("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.13.2")
+    api("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.13.2")
 
     api("com.typesafe:config:1.4.1")
     api("org.apache.commons:commons-lang3:3.9")
+
+    constraints {
+        api("com.fasterxml.jackson.core:jackson-databind:2.13.2.2")
+    }
 }
 
 java {
