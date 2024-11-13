b73efc2a03cefa4512579c27a0c0d6d62ebaae3e
kklongming <dragonsunmoon@qq.com>
2024-11-13 10:12:48 +0800

    增加用于格式化protobuf json 的 PrettyPbJsonFormatter

---
+++ examples/archetype/MyquantApiTest/src/main/kotlin/commons/PrettyPbJsonFormatter.kt
@@ -0,0 +1,16 @@
+package kts.tools
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.googlecode.protobuf.format.JsonJacksonFormat
+import java.io.OutputStream
+
+//
+// Created by drago on 2024/11/6 周三.
+//
+class PrettyPbJsonFormatter : JsonJacksonFormat() {
+    override fun createGenerator(output: OutputStream?): JsonGenerator {
+        val jsonGenerator = super.createGenerator(output)
+        jsonGenerator.useDefaultPrettyPrinter()
+        return jsonGenerator
+    }
+}
