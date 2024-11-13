39a8c9f29604c43e0d589263210ae21dfeb9dbb7
kklongming <dragonsunmoon@qq.com>
2024-11-13 10:13:07 +0800

    upgrade org.bouncycastle:bcprov-jdk18on to v1.78.1

--- subProjects/sz-crypto/build.gradle.kts
+++ subProjects/sz-crypto/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 }
 
 dependencies {
-    api("org.bouncycastle:bcprov-jdk18on:1.74")
+    api("org.bouncycastle:bcprov-jdk18on:1.78.1")
 
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
